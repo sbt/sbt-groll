10f1768a58792956d33a41c2b21e900b9925d231
Heiko Seeberger <mail@heikoseeberger.de>
2014-12-22 09:26:16 +0100

    Upgrade JGit to 3.5.3.201412180710-r (security update)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Version {
   val config    = "1.2.1"
-  val jGit      = "3.5.2.201411120430-r"
+  val jGit      = "3.5.3.201412180710-r"
   val scala     = "2.10.4"
   val scalaTest = "2.2.2"
 }
